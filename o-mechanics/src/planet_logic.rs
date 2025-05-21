3fcc802bd57b55237ce70bc96d94422e9ab43588
monkeyswift <131930835+monkeyswift@users.noreply.github.com>
2025-05-20 22:15:09 -0400

    Some restructuring
    Preparing to add a symplectic integrator to simulate the environment.

--- o-mechanics/src/planet_logic.rs
+++ o-mechanics/src/planet_logic.rs
@@ -1,18 +1,27 @@
 use bevy::prelude::*;
 use bevy::math::prelude::*;
 
-static GRAVITATIONAL_CONSTANT: f64 = 6.67430e-11;
-static STEP_SIZE: f64 = 1.0/160.0;
+static GRAVITATIONAL_CONSTANT: f32 = 6.67430e-11;
+static STEP_SIZE: f32 = 1.0/160.0;
 
 
 #[derive(Component)]
-pub struct Mass(f64);
+pub struct Mass(f32);
 
 #[derive(Component)]
 pub struct Velocity(Vec3);
 
 #[derive(Component)]
+pub struct Acceleration(Vec3);
+
+#[derive(Component)]
 pub struct Movable;
+
+#[derive(Component)]
+pub struct Focused; //This component exists purely to comply
+//with borrowing rules enforced by bevy and rust. I can't safely
+//access a query from two points at the same time unless I use
+//filters that help me break up the query into disjoint subsets.
 
 pub fn planet_setup(
     mut commands: Commands,
@@ -54,14 +63,65 @@ pub fn planet_setup(
 //concept, so I'm not concerned too much with accuracy
 //as I will be replacing this with a symplectic integrator.
 
-pub fn planet_update(mut query:
-    Query<&mut Transform,
+pub fn planet_update(mut parameters: ParamSet<(
+    mut mutable_query:
+    Query<(&mut Transform,
     Entity,
-    Velocity,
-    Mass,
-    With<Movable>>) {
-    for (mut transform, this_entity, this_velocity, this_mass) in query.iter_mut() {
-        transform.translation.x += 0.1;
-        transform.translation.y += 0.1;
+    &mut Velocity,
+    &mut Acceleration,
+    &Mass),
+    With<Movable>>,
+    Query<(&Transform,
+    Entity,
+    &Velocity, // should probably not be including these velocities.
+    &Acceleration,
+    &Mass),
+    With<Movable>,
+    >)
+    ) {
+
+        for (this_transform, 
+            this_entity, 
+            this_velocity, 
+            this_acceleration
+            this_mass
+        ) in parameters.p0().iter_mut()
+        {
+            for (other_transform,
+                other_entity,
+                other_velocity,
+                other_acceleration,
+                other_mass
+            ) in parameters.p1().iter()
+            {
+                let distance = other_transform.translation - this_transform.translation;
+                let force = GRAVITATIONAL_CONSTANT * (this_mass.0 * other_mass.0)/(distance.length_squared());
+                *this_acceleration = Acceleration(distance.normalize() * force/ this_mass.0);
+                //I know multiplying and dividing by mass is redundant, just keeping it for clarity.
+                *this_velocity = Velocity(this_velocity.0 + STEP_SIZE * this_acceleration.0);
+            }
+        }
+
+    //after the nested loop velocity update I do the 
+    //position update:
+    for (mut transform, _entity, velocity, acceleration, _mass) in mutable_query.iter_mut()
+    {
+        transform.translation += velocity.0*STEP_SIZE + 0.5 * acceleration.0 * STEP_SIZE.powi(2);
     }
 }
+
+fn calculate_forces(
+    ) {
+        for (other_transform,
+            other_entity,
+            _other_velocity,
+            _other_acceleration,
+            other_mass) in immutable_query.iter()
+                {
+                    let distance = other_transform.translation - this_transform.translation;
+                    let force = GRAVITATIONAL_CONSTANT * (this_mass.0 * other_mass.0)/(distance.length_squared());
+                    *this_acceleration = Acceleration(distance.normalize() * force/ this_mass.0);
+                    //I know multiplying and dividing by mass is redundant, just keeping it for clarity.
+                    *this_velocity = Velocity(this_velocity.0 + STEP_SIZE * this_acceleration.0);
+                }
+    }
